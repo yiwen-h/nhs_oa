0dd939602680921a44db273f5a97ca76be092824
YiWen Hon <yiwench@gmail.com>
2022-03-20 18:42:18 +0800

    added kwargs to get_pmids

--- nhs_oa/get_data.py
+++ nhs_oa/get_data.py
@@ -7,14 +7,16 @@ import requests
 
 
 # Documentation: https://www.ncbi.nlm.nih.gov/books/NBK25500/#chapter1.Downloading_Full_Records
-year = 2020
-base_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=nhs%5Baffiliation%5D+AND+{year}%5Bpdat%5D&retmax=50000&retmode=json"
 
-def get_pmids():
+def get_pmids(year = '2020', retmax = '20'):
+    """returns a list of PMIDs for a given search string"""
+    base_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=nhs%5Baffiliation%5D+AND+{year}%5Bpdat%5D&retmax={retmax}&retmode=json"
     re = requests.get(base_url).json()
     result = re["esearchresult"]["idlist"]
     return result
 
+def get_full_info(pmids):
+    pass
 
 
 
