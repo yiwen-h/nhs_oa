e6f5c3e131683c0d84a53ed6d9ee1a0f2684a499
YiWen Hon <yiwench@gmail.com>
2022-03-21 11:16:27 +0800

    small changes to tests

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -12,6 +12,9 @@ def test_get_doi():
     doi = get_data.get_doi()
     assert doi == "10.1007/s43465-021-00465-8"
 
+def test_get_full_info():
+    full_info = get_data.get_full_info()
+    assert type(full_info) == list and type(full_info[0] == dict)
 
 if __name__ == '__main__':
-    test_get_doi()
+    test_get_full_info()
