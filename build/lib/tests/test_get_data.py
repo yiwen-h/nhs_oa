69b70fb3578a612b2164e5037d4706be3571df11
YiWen Hon <yiwench@gmail.com>
2022-03-21 10:46:40 +0800

    added get doi function and test

--- build/lib/tests/test_get_data.py
+++ build/lib/tests/test_get_data.py
@@ -6,8 +6,12 @@ def test_get_pmids():
 
 def test_get_xml():
     art_xml = get_data.get_xml()
-    print(type(art_xml))
+    assert art_xml.tag == "PubmedArticleSet"
+
+def test_get_doi():
+    doi = get_data.get_doi()
+    assert doi == "10.1007/s43465-021-00465-8"
 
 
 if __name__ == '__main__':
-    test_get_xml()
+    test_get_doi()
